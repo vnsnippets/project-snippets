a0fc9cf96047314ebe9ffd84255b8b36e705a9c3
Vidush H. Namah <vn.developer@outlook.com>
2021-05-09 16:47:23 +0400

    blog layout

---
+++ layouts/blog.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="bg-gray-800 min-w-full min-h-screen">
+    <Navigation />
+    <Header />
+    <nuxt />
+    <Footer />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Header from '../components/Header.vue'
+
+export default Vue.extend({
+  layout: 'default',
+  components: { Header }
+})
+</script>
+
+<style>
+@media only screen and (max-width: 320px) {
+  html {
+    min-width: 320px;
+  }
+}
+</style>
