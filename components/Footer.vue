474ab00c17e713f374ffb7207143a9df0530092c
Vidush H. Namah <vn.developer@outlook.com>
2021-05-11 00:27:25 +0400

    reduce horizontal margins/paddings in footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="footer-container bg-black border-t-2 border-gray-800">
     <div class="p-5 bg-gray-900">
-      <div class="flex flex-col items-center justify-center p-5">
+      <div class="flex flex-col items-center justify-center py-5">
         <section class="mb-5">
           <h2 class="text-2xl" align="center">
             <span class="text-gray-600">VN</span>
@@ -9,9 +9,9 @@
           </h2>
         </section>
 
-        <section>
-          <div>
-            <nuxt-link v-for="e in links" :key="e.label" :to="e.url" class="text-gray-400 text-sm mx-3 uppercase">
+        <section align="center">
+          <div class="text-gray-400 text-sm uppercase">
+            <nuxt-link v-for="e in links" :key="e.label" :to="e.url" class="mx-3">
               {{ e.label }}
             </nuxt-link>
           </div>
@@ -22,7 +22,7 @@
     </div>
     <section class="py-5">
       <p class="text-gray-400 text-xs uppercase mb-3 text-center">
-        Articles and Projects created with <i class="cap-icon ci-heart mx-1"></i> from Mauritius
+        Articles and Projects created with <i class="cap-icon ci-heart mx-1"></i>
         <!-- THE BEST TECHNIQUES ARE PASSED ON BY THE SURVIVORS -->
       </p>
       <h6 class="text-gray-500 text-xs text-center">&copy; VIDUSH H. NAMAH ({{ new Date().getFullYear() }})</h6>
@@ -57,6 +57,14 @@ p {
   font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
 }
 
+a:first-of-type {
+   margin-left: 0;
+}
+
+a:last-of-type {
+   margin-right: 0;
+}
+
 a:hover {
   color: white;
   transition: all 0.2s ease-in-out;
@@ -64,7 +72,7 @@ a:hover {
 
 @media only screen and (max-width: 767px) {
   .footer-container {
-    padding-bottom: 4.25em;
+    padding-bottom: 3.5em;
   }
 }
 </style>
